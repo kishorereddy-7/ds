aa3a3f997ab513fe9c9a415dc0ef1ed9a3d5c227
Kishore <venkata.kishore@techstile.in>
2023-06-12 01:06:34 +0530

    double-done

--- doubleLinkedList.js
+++ doubleLinkedList.js
@@ -72,4 +72,73 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count, current;
+    if (index < this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count != index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count != index) {
+        current = current.prev;
+        count++;
+      }
+    }
+    return current;
+  }
+
+  set(index, val) {
+    let node = this.get(index);
+    if (node !== null) {
+      node.val = val;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) !!this.push(val);
+    if (index === 0) !!this.unshift(val);
+
+    let newNode = new Node(val);
+    let indexNode = this.get(index);
+    let prevNode = indexNode.prev;
+
+    prevNode.next = newNode;
+    indexNode.prev = newNode;
+    newNode.prev = prevNode;
+    newNode.next = indexNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) this.pop();
+
+    let removeNode = this.get(index);
+    let prevNode = removeNode.prev;
+    let nextNode = removeNode.next;
+
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
+
+    this.removeNode.next = null;
+    this.removeNode.prev = null;
+    this.length--;
+    return true;
+  }
 }
+
+const a = new DoublyLinkedList();
+a.push(1);
+a.push(2);
+a.push(3);
