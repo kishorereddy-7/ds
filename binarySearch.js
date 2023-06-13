9f553c0bfa1cd5424031c96d09878bb04b0ffa4f
Kishore <venkata.kishore@techstile.in>
2023-06-14 00:15:53 +0530

    binary-search-tree

---
+++ binarySearch.js
@@ -0,0 +1,59 @@
+class BNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    const newNode = new BNode(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  find(val) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (!found && current) {
+      if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    return found ? current : undefined;
+  }
+}
+
+const tree = new BinarySearchTree();
